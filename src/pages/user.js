9e9f6ffeb63d081f6f1cfff1ce3bd5a816619b97
Eduardo Martin <lalom1227@gmail.com>
2024-06-18 21:08:48 -0600

    demo

---
+++ src/pages/user.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Head from 'next/head';
+import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
+import RecipeReviewCard from 'src/components/userPost';
+import Postx2 from 'src/components/postsPost';
+
+const Page = () => {
+  return (
+    <>
+      <Head>
+        <title>Twiter2</title>
+      </Head>
+
+      <DashboardLayout>
+        <Postx2 />
+        <RecipeReviewCard />
+      </DashboardLayout>
+    </>
+  );
+};
+
+export default Page;
