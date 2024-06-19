d89e195bfcb357cbe1f8e8b919345fd392fb5678
Eduardo Martin <lalom1227@gmail.com>
2024-06-18 21:10:02 -0600

    demo

--- src/layouts/dashboard/layout.js
+++ src/layouts/dashboard/layout.js
@@ -1,26 +1,24 @@
-import { useCallback, useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import { styled } from "@mui/material/styles";
-import { withAuthGuard } from "src/hocs/with-auth-guard";
-import { TopNav } from "./top-nav";
-import { CatalogsProvider } from "src/contexts/catalogs-context";
+import { useCallback, useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { styled } from '@mui/material/styles';
+import { withAuthGuard } from 'src/hocs/with-auth-guard';
+import { TopNav } from './top-nav';
+import { CatalogsProvider } from 'src/contexts/catalogs-context';
 
 const SIDE_NAV_WIDTH = 280;
 
-const LayoutRoot = styled("div")(({ theme }) => ({
-  display: "flex",
-  flex: "1 1 auto",
-  maxWidth: "100%",
-  [theme.breakpoints.up("lg")]: {
+const LayoutRoot = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flex: '1 1 auto',
+  maxWidth: '100%',
+  [theme.breakpoints.up('lg')]: {
     paddingLeft: SIDE_NAV_WIDTH,
   },
 }));
 
-const LayoutContainer = styled("div")({
+const LayoutContainer = styled('div')({});
 
-});
-
-export const Layout = withAuthGuard((props) => {
+export const Layout = withAuthGuard(props => {
   const { children } = props;
   const pathname = usePathname();
   const [openNav, setOpenNav] = useState(false);
@@ -31,13 +29,9 @@ export const Layout = withAuthGuard((props) => {
     }
   }, [openNav]);
 
-  useEffect(
-    () => {
-      handlePathnameChange();
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [pathname]
-  );
+  useEffect(() => {
+    handlePathnameChange();
+  }, [pathname]);
 
   return (
     <>
